a2e91cac90e3babd925b0a5f75cf0ffbde220024
Steve Hannah <steve@weblite.ca>
2017-11-28 10:38:14 -0800

    Set new release version to 0.5.0-cn1002

--- merge-master.sh
+++ merge-master.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if !(git remote | grep upstream > /dev/null); then
-  git remote add upstream https://github.com/konsoletyper/teavm.git
-fi
-git checkout master
-git fetch upstream
-git merge upstream/master
-git push origin master
+# if !(git remote | grep upstream > /dev/null); then
+#   git remote add upstream https://github.com/konsoletyper/teavm.git
+# fi
+# git checkout master
+# git fetch upstream
+# git merge upstream/master
+# git push origin master
